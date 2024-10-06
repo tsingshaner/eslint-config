54ccb097d9121ac1a0ee681bf040de26ab64277b
Jon-a-than <2140145186@qq.com>
2024-10-06 22:09:06 +0800

    fix: fix eslint-plugin-react dts error

---
+++ .changeset/unlucky-suits-push.md
@@ -0,0 +1,5 @@
+---
+"@qingshaner/eslint-config": patch
+---
+
+Fix eslint-plugin-react dts error
--- src/configs/react.ts
+++ src/configs/react.ts
@@ -1,8 +1,6 @@
-// @ts-expect-error:7016 no types for eslint-plugin-react
 import reactPlugin from 'eslint-plugin-react'
-// import * as reactHooksPlugin from 'eslint-plugin-react-hooks'
 
-import type { Linter } from 'eslint'
+import type { ESLint, Linter } from 'eslint'
 
 import { GLOB_SRC } from '../globs'
 
@@ -46,10 +44,6 @@ export interface ReactOverrideOptions {
   }
 }
 
-interface ReactPlugin {
-  configs: Record<'jsx-runtime' | 'recommended', Linter.Config>
-}
-
 export const defineReactConfig = ({
   files = [GLOB_SRC],
   rules,
@@ -60,13 +54,13 @@ export const defineReactConfig = ({
       files: files,
       name: 'qingshaner/react',
       plugins: {
-        react: reactPlugin as ReactPlugin
+        react: reactPlugin.configs.flat.recommended.plugins.react as ESLint.Plugin
         // 'react-hooks': is implemented in Biome, so we can't use it directly
         // 'react-refresh': is implemented in Biome, so we can't use it directly
       },
       rules: {
-        ...(reactPlugin as ReactPlugin).configs.recommended.rules,
-        ...(reactPlugin as ReactPlugin).configs['jsx-runtime'].rules,
+        ...(reactPlugin.configs.flat.recommended.rules as ReactRuleOptions),
+        ...(reactPlugin.configs.flat['jsx-runtime'].rules as ReactRuleOptions),
         ...rules
       },
       settings: {
